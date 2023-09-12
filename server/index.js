a7211b0c8c90469a893e5e4eb55d5411d2f18fa5
Laxman B <laxmanbalaraman@gmail.com>
2023-09-12 21:05:50 +0530

    Correct some importing issues and renaming of few files

--- server/index.js
+++ server/index.js
@@ -10,9 +10,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/user.js'
-import postRoutes from '/routes/posts.js';
+import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
-import { createPost } from './controllers/posts/js';
+import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
 
 // CONFIGURATIONS
