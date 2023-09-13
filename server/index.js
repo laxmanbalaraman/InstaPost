a67ab031ba0cb5d328d85574131a94568a74fba4
Laxman B <laxmanbalaraman@gmail.com>
2023-09-13 19:30:55 +0530

    add mock data

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
 import { createPost } from './controllers/posts.js';
 import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 // CONFIGURATIONS
 
@@ -66,6 +69,10 @@ try{
     });
 
     app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+
+    // Mock data
+    // User.insertMany(users);
+    // Post.insertMany(posts);
 }
 
 catch(error){
